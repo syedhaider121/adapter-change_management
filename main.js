8cd478e4e32410fbb93d31587862fe6f4f09effa
syedhaider121 <syedhaider121@yahoo.com>
2020-10-29 22:45:51 +0000

    Final Project updates

--- main.js
+++ main.js
@@ -80,7 +80,6 @@ class ServiceNowAdapter extends EventEmitter {
   connect(callback) {
     // As a best practice, Itential recommends isolating the health check action
     // in its own method.
-
     this.healthcheck(callback);
   }
 
@@ -95,41 +94,15 @@ class ServiceNowAdapter extends EventEmitter {
    *   that handles the response.
    */
   healthcheck(callback) {
-    this.getRecord((result, error) => {
-    /**
-        * For this lab, complete the if else conditional
-        * statements that check if an error exists
-        * or the instance was hibernating. You must write
-        * the blocks for each branch.
-        */
+    this.connector.get((result, error) => {
     if (error) {
-        /**
-        * Write this block.
-        * If an error was returned, we need to emit OFFLINE.
-        * Log the returned error using IAP's global log object
-        * at an error severity. In the log message, record
-        * this.id so an administrator will know which ServiceNow
-        * adapter instance wrote the log message in case more
-        * than one instance is configured.
-        * If an optional IAP callback function was passed to
-        * healthcheck(), execute it passing the error seen as an argument
-        * for the callback's errorMessage parameter.
-        */
         this.emitOffline();
+        callback.errorMessage = error;
         log.error("ServiceNow: Instance has issue for Id: " + this.id + " with error: "+  error);
     } else {
-        /**
-        * Write this block.
-        * If no runtime problems were detected, emit ONLINE.
-        * Log an appropriate message using IAP's global log object
-        * at a debug severity.
-        * If an optional IAP callback function was passed to
-        * healthcheck(), execute it passing this function's result
-        * parameter as an argument for the callback function's
-        * responseData parameter.
-        */
         this.emitOnline();
-        log.info("ServiceNow: Instance has no issues for Id: " + this.id + " with result: "+  JSON.stringify(result));
+        callback.responseData = result;
+        log.info("ServiceNow: Instance has no issues for Id: " + this.id + " with result.statusCode: "+  result.statusCode);
     }
     });
   }
